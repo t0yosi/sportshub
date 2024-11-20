f05b01a65745cdccf10fe2d228ace495ce2020bf
t0yosi <177050929+t0yosi@users.noreply.github.com>
2024-11-20 09:49:01 +0100

    start project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import React from "react";
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
